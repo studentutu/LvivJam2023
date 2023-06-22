39b54d1575aab2988e4d3532ba0b0211cf372547
Roman Fedorov <r.fedorov@plarium.com>
2023-06-22 20:02:34 +0300

    Add camera

--- Jam/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Jam/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -207,7 +207,8 @@ namespace StarterAssets
             _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
 
             // Cinemachine will follow this target
-            CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
+            // _cinemachineTargetPitch + CameraAngleOverride
+            CinemachineCameraTarget.transform.rotation = Quaternion.Euler(0, // will ignore X rotation
                 _cinemachineTargetYaw, 0.0f);
         }
 
