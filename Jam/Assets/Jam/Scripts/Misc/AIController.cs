7494bc12779be3d98b08c935398a968d6c5a5dd5
Roman Fedorov <r.fedorov@plarium.com>
2023-06-24 01:10:12 +0300

    FIxed patrol

--- Jam/Assets/Jam/Scripts/Misc/AIController.cs
+++ Jam/Assets/Jam/Scripts/Misc/AIController.cs
@@ -17,14 +17,14 @@ public class AIController : MonoBehaviour
     float m_WaitTime; //  Variable of the wait time that makes the delay
     bool m_IsPatrol; //  If the enemy is patrol, state of patroling
 
+    private CompositeDisposable _disposable = new();
+
     void Start()
     {
         m_IsPatrol = true;
         m_WaitTime = startWaitTime; //  Set the wait time variable that will change
 
         m_CurrentWaypointIndex = 0; //  Set the initial waypoint
-
-        navMeshAgent.isStopped = true;
     }
 
     private Waypoints _waypoints;
@@ -35,11 +35,14 @@ public class AIController : MonoBehaviour
         {
             _waypoints = x;
 
-            navMeshAgent.isStopped = false;
             navMeshAgent.speed = speedWalk; //  Set the navemesh speed with the normal speed of the enemy
-            navMeshAgent.SetDestination(_waypoints.waypoints[m_CurrentWaypointIndex]
-                .position); //  Set the destination to the first waypoint
-        }).Dispose();
+            NextPoint();
+        }).AddTo(_disposable);
+    }
+
+    private void OnDisable()
+    {
+        _disposable?.Dispose();
     }
 
     private void Update()
@@ -52,19 +55,16 @@ public class AIController : MonoBehaviour
 
     private void Patroling()
     {
-        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        m_WaitTime -= Time.deltaTime;
+        //  If the enemy arrives to the waypoint position then wait for a moment and go to the next
+        if (m_WaitTime <= 0 || navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
         {
-            m_WaitTime -= Time.deltaTime;
-            //  If the enemy arrives to the waypoint position then wait for a moment and go to the next
-            if (m_WaitTime <= 0)
-            {
-                NextPoint();
-                m_WaitTime = startWaitTime;
-            }
+            NextPoint();
+            m_WaitTime = startWaitTime;
         }
     }
 
-    public void NextPoint()
+    private void NextPoint()
     {
         m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % _waypoints.waypoints.Length;
         navMeshAgent.SetDestination(_waypoints.waypoints[m_CurrentWaypointIndex].position);
